2a4b452059e23ede194edf493e88aeca2604309a
Ozodbek Mamaruziyev <ozodbekm84@gmail.com>
2025-03-29 16:56:36 +0500

    Birinchi commit

--- main.js
+++ main.js
@@ -1,57 +1,44 @@
-const API_KEY = 'dcea1fd7b3e65d34387ad6de7ef9cc5e'
-const tokenTop = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`
-const tokenPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-const tokenUpComing = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`
+let API_KEY = 'dcea1fd7b3e65d34387ad6de7ef9cc5e'
 
-let move = {
-    top_rated: tokenTop, popular: tokenPopular, upcoming: tokenUpComing
+let tokenTop = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`
+let tokenPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+let tokenUpComing = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`
+
+let append = document.querySelector('.append');
+
+function getMovies(el) {
+    fetch(el)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.results)
+            showMovies(data.results)
+        })
+        .catch(error => console.error(error))
 }
 
-let addMove = document.querySelector('.append')
-
-let div = document.createElement('div')
-
-// 
-document.getElementById('top').addEventListener('click',() => 
-    moves('top_rated'))
-document.getElementById('popular').addEventListener('click', () => 
-    moves('popular'))
-
-document.getElementById('upcoming').addEventListener('click', () => 
-    moves('upcoming'))
-
-
-
-
-
-
-let moves = async (el) =>{
-
-    const response = await fetch(move[el]);
-    const data = await response.json();
-    renderMovies(data.results);
-}
-
-let renderMovies = (movies) => {
-    addMove.innerHTML = ''
-
+function showMovies(movies) {
+    append.innerHTML = '';
     movies.forEach(movie => {
-        
-        div = document.createElement('div')
-        div.classList.add('movie')
-        
-        div.innerHTML = `
+        let movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+        movieElement.innerHTML = `
             <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
             <div class="movie-info">
                 <h3>${movie.title}</h3>
                 <span class="orange">${movie.vote_average}</span>
             </div>
             <span class="date">${movie.release_date}</span>
-        `
-        
-        addMove.appendChild(div);
+        `;
+        append.appendChild(movieElement);
     });
-};
+}
 
+document.querySelectorAll('.btns').forEach(button => {
 
-moves('top_rated');
+    button.addEventListener('click', () => {
+        if (button.value === 'top_rated') getMovies(tokenTop)
+        else if (button.value === 'popular') getMovies(tokenPopular)
+    })
+})
+
+getMovies(tokenTop);
